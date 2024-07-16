1103019ed078012bab56d48d69f90cd9153c7683
mikhailyanin <mikebagley17@gmail.com>
2024-07-16 06:23:34 +0300

    Реализован сервис для отправки заявки для вступления в клуб

---
+++ frontend/src/app/models/club.model.ts
@@ -0,0 +1,11 @@
+export interface IValueClub {
+    id: number;
+    text: string;
+  }
+
+  export interface IClub {
+    id: number;
+    name: string;
+    descriprion: string;
+    img: string;
+}
