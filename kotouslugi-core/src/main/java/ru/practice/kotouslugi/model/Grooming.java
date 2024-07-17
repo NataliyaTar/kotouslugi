68e677553201e7fc5b26473dab61b8369c3c0b54
ВашÐAnimeLLLHNK <.emkomaroffartem2014@gmail.com>
2024-07-17 21:31:12 +0300

    1. Убрал лишние импорты 2. Убрал лишние отступы 3. description поправил 4. Поправлена табуляция

--- kotouslugi-core/src/main/java/ru/practice/kotouslugi/model/Grooming.java
+++ kotouslugi-core/src/main/java/ru/practice/kotouslugi/model/Grooming.java
@@ -1,12 +1,9 @@
 package ru.practice.kotouslugi.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import ru.practice.kotouslugi.model.enums.Sex;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -19,9 +16,11 @@ import jakarta.persistence.Table;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Grooming {
+
   @Id
   @GeneratedValue
   private Long id;
+
   private String cat;
   private String phoneNumber;
   private String email;
@@ -29,4 +28,3 @@ public class Grooming {
   private String date;
   private String time;
 }
-
--- kotouslugi-core/src/main/java/ru/practice/kotouslugi/service/GroomingService.java
+++ kotouslugi-core/src/main/java/ru/practice/kotouslugi/service/GroomingService.java
@@ -1,12 +1,10 @@
 package ru.practice.kotouslugi.service;
 
-
 import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.practice.kotouslugi.dao.GroomingRepository;
 import ru.practice.kotouslugi.model.Grooming;
-
 
 @Service
 public class GroomingService {
@@ -18,21 +16,14 @@ public class GroomingService {
     this.groomingRepository = groomingRepository;
   }
 
-
-
   public Long addGrooming(Grooming grooming) {
-
     try {
       grooming = groomingRepository.save(grooming);
-      logger.info(String.format("Добавлен запись на груминг = %s ", grooming.getId()));
+      logger.info(String.format("Добавлена запись на груминг = %s ", grooming.getId()));
       return grooming.getId();
     } catch (Exception e) {
       logger.error(e.getMessage());
       return null;
     }
-
   }
-
 }
-
-
