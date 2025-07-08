045b25beccb35a47e09a18eaabd550f4ca6c96ed
dencamenew <dencamenew@yandex.ru>
2025-07-08 16:27:02 +0300

    create tables in tadadase

---
+++ kotouslugi-core/src/main/java/ru/practice/kotouslugi/model/Metrics.java
@@ -0,0 +1,30 @@
+package ru.practice.kotouslugi.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Builder
+@Table(name = "metrics")
+public class Metrics {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Long id;
+
+  @Column(name = "status")
+  private Boolean status;
+
+  @Column(name = "date_start")
+  private LocalDateTime dateStart;
+
+  @Column(name = "date_end")
+  private LocalDateTime dateEnd;
+}
