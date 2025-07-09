9ee9f397d5cefcee99194fbfe6d7528c22f3f14e
iDontKnowMyNickname <rdburtsev@gmail.com>
2025-07-09 12:13:42 +0300

    Добавить FitnessController
    Добавлен FitnessController, а так же FitnessService, FitnessRepository.
    FitnessController предоставляет функционал для получения списка фитнес-клубов, добавления, удаления и получения фитнес-клуба по ID.
    
    Оформлена документация по работе с API (см. Swagger, пункты "FitnessController" и "API фитнес-клубов").

---
+++ kotouslugi-core/src/main/java/ru/practice/kotouslugi/service/FitnessService.java
@@ -0,0 +1,69 @@
+package ru.practice.kotouslugi.service;
+
+import org.springframework.stereotype.Service;
+import ru.practice.kotouslugi.dao.FitnessRepository;
+import ru.practice.kotouslugi.model.Fitness;
+import ru.practice.kotouslugi.request.RequestId;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+/** Сервис для работы со списком фитнес-клубов
+ *
+ * @author Роман Бурцев
+ * @author Свободные места
+ */
+public class FitnessService {
+    private final FitnessRepository fitnessRepository;
+    private static final Logger logger = LoggerFactory.getLogger(FitnessService.class);
+
+    public FitnessService(FitnessRepository fitnessRepository) {
+        this.fitnessRepository = fitnessRepository;
+    }
+
+    /** Получение списка всех фитнес-клубов
+     * @return - список фитнес-клубов
+     */
+    public List<Fitness> listFitness() {
+        List<Fitness> entityList = new LinkedList<>();
+        Iterable<Fitness> fitnessEntities = fitnessRepository.findAll();
+        fitnessEntities.forEach(entityList::add);
+        return entityList;
+    }
+
+    /** Получение фитнес-клуба по его id
+     *
+     * @param id - id фитнес-клуба
+     * @return искомый фитнес-клуб
+     */
+    public Fitness getFitness(Long id) {
+        Optional<Fitness> fitness = fitnessRepository.findById(id);
+        return fitness.orElse(null);
+    }
+
+    /** Добавление фитнес-клуба
+     *
+     * @param fitness - объект с данными нового фитнес-клуба
+     * @return идентификатор созданной записи
+     */
+    public Long addFitness(Fitness fitness) {
+        try {
+            fitness = fitnessRepository.save(fitness);
+            logger.info(String.format("Добавлен фитнес-клуб №%d = %s ", fitness.getId(), fitness.getFitness_club()));
+            return fitness.getId();
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return null;
+        }
+    }
+
+    public void deleteFitness(Long id) {
+      Optional<Fitness> fitness = fitnessRepository.findById(id);
+      fitness.ifPresent(fitnessRepository::delete);
+    }
+}
