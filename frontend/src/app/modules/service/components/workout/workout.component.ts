7046339acd679869f170f77239c4040b80df3b55
BuggsCreator <asushkovv@mail.ru>
2025-07-12 21:20:13 +0300

    исправлены баги

--- frontend/src/app/modules/service/components/workout/workout.component.ts
+++ frontend/src/app/modules/service/components/workout/workout.component.ts
@@ -70,7 +70,7 @@ export class WorkoutComponent implements OnInit, OnDestroy {
     private serviceInfo: ServiceInfoService
   ) {}
 
-  ngOnInit(): void {
+    ngOnInit(): void {
     this.getCatOption();
 
     this.constantService
@@ -119,7 +119,7 @@ export class WorkoutComponent implements OnInit, OnDestroy {
                 const initialSelected = this.fitnessFullList.find(
                   (f) => f.id === initialFitnessId
                 );
-                this.form.get('2.price')?.setValue(initialSelected?.price);
+                this.form.get('1.price')?.setValue(initialSelected?.price);
               });
           });
       });
@@ -184,7 +184,6 @@ export class WorkoutComponent implements OnInit, OnDestroy {
       2: this.fb.group({
         telephone: ['', [Validators.required, Validators.pattern(/^\d{11}$/)]],
         email: ['', [Validators.email]],
-        price: [null],
       }),
     });
 
