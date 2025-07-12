f07f62170997587c3530c94fd623c65bc14bc0bb
Daniil Lifatov <2004daniil9015@mail.ru>
2025-07-12 23:26:43 +0300

    Feedbacks are recorded using the API, pre-finished recording selection

--- frontend/src/app/modules/feedbacks/feedbacks.component.ts
+++ frontend/src/app/modules/feedbacks/feedbacks.component.ts
@@ -6,6 +6,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { HttpClient } from '@angular/common/http'; // Добавьте этот импорт
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { OrderService } from '@services/order/order.service';
+import { EStatus, IOrder, TStatus } from '@models/order.model';
 
 @Component({
   selector: 'app-feedbacks',
@@ -21,10 +23,30 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./feedbacks.component.scss']
 })
 
-export class FeedbacksComponent {
+export class FeedbacksComponent implements OnInit {
+    public loading = true; // Загружены ли данные для страницы
+      public error = false; // Произошла ли ошибка реста
     public rating: number;
     public comment: String;
-    constructor(private http: HttpClient) {}
+    public orders: IOrder[]; // Список заявок
+
+    constructor(
+        private orderService: OrderService,
+      ) {
+      }
+
+    public ngOnInit() {
+        // получаем список заявок
+        this.orderService.getOrdersList().subscribe(res => {
+          this.orders = res;
+          this.loading = false;
+        }, error => {
+          this.loading = false;
+          this.error = true;
+        })
+      }
+
+
      submitFeedback() {
         if (this.rating === null || this.comment.trim() === '') {
           alert('Пожалуйста, заполните все поля');
@@ -47,7 +69,7 @@ export class FeedbacksComponent {
           body: jsonData
         })
 
-        .then(response => response.json())
+        .then(response => response.text())
         .then(data => {
           console.log('Ответ сервера:', data);
         })
