e8f4e5f3fd8525947e1e0a52ce4ffb81444d169d
89802663834 <savkov.name@yandex.ru>
2025-07-14 18:49:24 +0300

    Add files via upload

---
+++ service.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ServiceComponent } from './service.component';
+import { NewFamilyComponent } from './components/new-family/new-family.component';
+import { VetComponent } from './components/vet/vet.component';
+import { GroomingComponent } from './components/grooming/grooming.component'; // ← Измените ../ на ./
+
+/**
+ * Роутинг для услуг
+ */
+const routes: Routes = [
+  {
+    path: '',
+    component: ServiceComponent,
+    children: [
+      {
+        path: 'new_family',
+        pathMatch: 'full',
+        data: {
+          idService: 'new_family'
+        },
+        component: NewFamilyComponent
+      },
+      {
+        path: 'vet',
+        pathMatch: 'full',
+        data: {
+          idService: 'vet'
+        },
+        component: VetComponent
+      },
+      // ToDo: your router for service
+      {
+        path: 'grooming',
+        component: GroomingComponent,
+        data: { idService: 'grooming' } // важно для общих сервисов
+      }
+    ]
+  }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forChild(routes),
+  ]
+})
+export class ServiceModule { }
