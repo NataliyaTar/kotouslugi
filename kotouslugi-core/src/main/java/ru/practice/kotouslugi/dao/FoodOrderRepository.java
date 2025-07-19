36a4fe8895f3da511223856142765620514247b7
Дмитрий <dmitrij.dementeev.05@mail.ru>
2025-07-19 17:49:17 +0300

    Убрал бэкенд, так как он не работал. Фронт функционирует

--- kotouslugi-core/src/main/java/ru/practice/kotouslugi/dao/FoodOrderRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.practice.kotouslugi.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.practice.kotouslugi.model.FoodOrder;
-
-public interface FoodOrderRepository extends JpaRepository<FoodOrder, Long> {
-}
