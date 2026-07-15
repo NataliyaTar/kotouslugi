88e58cb580f9bcf2e5626b53894c33172e705dc7
kirill4ch3 <kryakrya2007.55@gmail.com>
2026-07-15 21:52:59 +0300

    добавил поллинг раз в 2 минуты для демонстрации работы условных сотрудников, не тестил

--- kotouslugi-core/src/main/java/ru/practice/kotouslugi/dao/RequisitionRepository.java
+++ kotouslugi-core/src/main/java/ru/practice/kotouslugi/dao/RequisitionRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import ru.practice.kotouslugi.model.Requisition;
 
-public interface RequisitionRepository extends CrudRepository<Requisition, Integer> {
+import java.util.List;
+import java.util.Optional;
 
+public interface RequisitionRepository extends CrudRepository<Requisition, Integer> {
+    List<Requisition> findAll();
 }
---
+++ kotouslugi-core/src/main/java/ru/practice/kotouslugi/service/PollingService.java
@@ -0,0 +1,33 @@
+package ru.practice.kotouslugi.service;
+
+
+import lombok.Data;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import ru.practice.kotouslugi.dao.RequisitionRepository;
+import ru.practice.kotouslugi.model.DecisionPassportDTO;
+import ru.practice.kotouslugi.model.Requisition;
+
+import java.util.List;
+
+//в качестве демонстрации работы реальных сотрудников, будет происходить каждые 2 минуты подтверждение заявок на паспорт
+@Data
+@Service
+public class PollingService {
+
+  private final RequisitionService requisitionService;
+  private final RequisitionRepository requisitionRepository;
+  private final CatPassportService catPassportService;
+
+  @Scheduled(fixedDelay = 120_000)
+  public void approveReq2min(){
+    List<Requisition> requisitionList = requisitionRepository.findAll();
+
+    if (!requisitionList.isEmpty()){
+      for (Requisition req : requisitionList){
+        DecisionPassportDTO decisionPassportDTO = new DecisionPassportDTO(req.getId());
+        catPassportService.approvePassport(decisionPassportDTO);
+      }
+    }
+  }
+}
