6cba0b2bb7f49c69defe708584829817ecd2af5d
sidelnikovamaria <145535838+sidelnikovamaria@users.noreply.github.com>
2026-07-29 20:38:39 +0300

    Revert "2026-summer-team-dedlayn-vchera"

--- kotouslugi-core/src/main/java/ru/practice/kotouslugi/dao/BreedingRequestRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.practice.kotouslugi.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.practice.kotouslugi.model.BreedingRequest;
-
-@Repository
-public interface BreedingRequestRepository extends JpaRepository<BreedingRequest, Long> {
-}
